f28d4db9ede1418a3457ac01888e691d07eb2359
David Jessup <djessup@users.noreply.github.com>
2025-03-16 20:13:03 +1100

    refactor: restructure security settings to improve organization of PAM services

--- darwin/settings/security.nix
+++ darwin/settings/security.nix
@@ -1,10 +1,12 @@
 { config, pkgs, ... }:
 {
-  security.pam.services.sudo_local = {
-    # Enable biometric auth for sudo
-    touchIdAuth = true;
-    watchIdAuth = true;
-    # This allows programs like tmux and screen that run in the background to survive across user sessions to work with PAM services that are tied to the bootstrap session.
-    reattach = true;
+  security = {
+    pam.services.sudo_local = {
+      # Enable biometric auth for sudo
+      touchIdAuth = true;
+      watchIdAuth = true;
+      # This allows programs like tmux and screen that run in the background to survive across user sessions to work with PAM services that are tied to the bootstrap session.
+      reattach = true;
+    };
   };
 }
