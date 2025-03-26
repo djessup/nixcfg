bd7fc1ede5609a76566cc02da631fd1c8ac6ac8f
David Jessup <djessup@users.noreply.github.com>
2025-03-27 10:42:10 +1100

    fix: remove broken golangci_lint_ls plugin (also i don't care)

--- user/settings/neovim/plugins/lsp.nix
+++ user/settings/neovim/plugins/lsp.nix
@@ -24,7 +24,7 @@
 
         servers = {
           gopls.enable = true;
-          golangci_lint_ls.enable = true;
+          # golangci_lint_ls.enable = true;
           lua_ls.enable = true;
           nil_ls.enable = true;
           pyright.enable = true;
