7e141d97a00d946f93cd3258f7980845a35e8651
David Jessup <djessup@users.noreply.github.com>
2026-03-03 22:53:43 +1100

    feat(zsh): evaluating atuin shell history manager

--- user/settings/zsh.nix
+++ user/settings/zsh.nix
@@ -169,4 +169,14 @@
       ];
     };
   };
+
+  programs.atuin = {
+    enable = false;
+    settings = {
+      auto_sync = true;
+      sync_frequency = "5m";
+      sync_address = "https://api.atuin.sh";
+      search_mode = "fuzzy";
+    };
+  };
 }
